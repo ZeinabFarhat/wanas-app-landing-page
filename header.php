fcf1f707d10b19d5762e7cec3f210731f9faf1df
Dana <dana.nada@mmpww.com>
2022-09-24 01:37:51 +0300

    first commit

--- header.php
+++ header.php
@@ -1,16 +1,51 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="author" content="Strategies dC" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="author" content="Strategies dC"/>
 
-	<title>Test</title>
+    <title>Wanas App</title>
 
-	<link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="/style.css"/>
 </head>
 <body>
 
 <!-- Document Wrapper
 ============================================= -->
 <div id="wrapper" class="clearfix">
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col col-1">
+                    <div class="star star-1">
+                        <img src="assets/img/star.png" alt="Star">
+                    </div>
+                </div>
+                <div class="col col-5 pl-5">
+                    <div class="logo">
+                        <img src="assets/img/header/logo.png" alt="Logo">
+                    </div>
+                    <div class="header-text">Experience Eastern Province Festive</div>
+                    <div class="header-text-2">Wanas shall provide Audience in Western Province with a new guided activity booking experience</div>
+                    <div class="row">
+                        <div class="button">
+                            <img src="assets/img/button.png" alt="Button">
+                        </div>
+                        <div class="button">
+                            <img src="assets/img/button-2.png" alt="Button">
+                        </div>
+                    </div>
+                    <div class="star star-2">
+                        <img src="assets/img/star.png" alt="Star">
+                    </div>
+                </div>
+                <div class="col col-6">
+                    <div class="ellipse">
+                        <img src="assets/img/header/side.png" alt="Ellipse">
+                    </div>
+                    <a href="#" class="btn download-button"> <img src="assets/img/header/button-3.png" alt="Download"></a>
+                </div>
+            </div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,3 +1,205 @@
 <?php include_once "header.php"; ?>
 
+    <section class="section-1 mt-5">
+        <div class="row justify-content-center align-middle">
+            <div class="text-2 text-center">Platform Features</div>
+            <div class="dash"><img src="assets/img/dash.png" alt="Dash"></div>
+        </div>
+        <div class="features align-items-center">
+            <div class="container">
+                <div class="row p-0 m-0">
+                    <div class="col col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="card align-items-center border-0 shadow">
+                            <div class="logo">
+                                <img src="assets/img/features/logo-1.png" alt="Logo">
+                            </div>
+                            <div class="card-body text-center mb-2">
+                                <div class="card-title">
+                                    Customer Support
+                                </div>
+                                <div class="card-text">
+                                    User will receive in app support on tickets booking and events listing.
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="card align-items-center border-0 shadow">
+                            <div class="logo">
+                                <img src="assets/img/features/logo-2.png" alt="Logo">
+                            </div>
+                            <div class="card-body text-center mb-2">
+                                <div class="card-title">
+                                    Alerts and Updates
+                                </div>
+                                <div class="card-text">
+                                    Wanas will alert the user on activity updates, planner updates as well as alerts on event time.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="card align-items-center border-0 shadow">
+                            <div class="logo">
+                                <img src="assets/img/features/logo-3.png" alt="Logo">
+                            </div>
+                            <div class="card-body text-center mb-2">
+                                <div class="card-title">
+                                    Online Payment
+                                </div>
+                                <div class="card-text">
+                                    Wanas integrate choices of multiple gateways available for integration. Users will complete booking ticket purchase from the app.
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xs-6 col-sm-6 col-md-6  col-lg-3 col-xl-3">
+                        <div class="card align-items-center border-0 shadow">
+                            <div class="logo">
+                                <img src="assets/img/features/logo-4.png" alt="Logo">
+                            </div>
+                            <div class="card-body text-center mb-2">
+                                <div class="card-title">
+                                    Special booking Offers
+                                </div>
+                                <div class="card-text">
+                                    All tickets added to wanas can include offers and discounts based on organizer plans and at anytime during the event period.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center align-middle mt-5">
+            <div class="features-button"><img src="assets/img/features/button.png" alt="Button"></div>
+        </div>
+    </section>
+
+    <section class="section-2 mt-5">
+        <div class="row">
+            <div class="star">
+                <img src="assets/img/star.png" alt="Star">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-7">
+                <div class="ellipse mt-3">
+                    <img src="assets/img/plan/ellipse-2.png" alt="Ellipse">
+                </div>
+            </div>
+            <div class="col col-5 mt-5">
+                <div class="side-section">
+                    <div class="text">Plan your fun time</div>
+                    <div class="dash"><img src="assets/img/dash.png" alt="Dash"></div>
+                    <div class="title-checkbox mt-4">
+                        <div class="checkbox">
+                            <img src="assets/img/plan/checkbox.png" alt="Checkbox">
+                        </div>
+                        <div class="text-title align-middle">Best Fit Recommendations</div>
+                        <div class="text-body">Wanas provide users with ability to set a day worth of activities based on choices and preferences. The app calendar will help user to fill the day
+                            with activities and break time options.
+                        </div>
+                    </div>
+                    <div class="title-checkbox mt-4">
+                        <div class="checkbox">
+                            <img src="assets/img/plan/checkbox.png" alt="Checkbox">
+                        </div>
+                        <div class="text-title align-middle">Track Fun Plan</div>
+                        <div class="text-body">Wanas provide users with ability to set a day worth of activities based on choices and preferences. The app calendar will help user to fill the day
+                            with activities, recommended dining to add on the day plan.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="section-3 mt-5">
+        <div class="top">
+            <img src="assets/img/booking/top.png" alt="Top">
+        </div>
+        <div class="bottom">
+            <div class="container">
+                <div class="row justify-content-center align-middle">
+                    <div class="text-2 text-center">New Booking Experience</div>
+                    <div class="dash"><img src="assets/img/dash.png" alt="Dash"></div>
+                </div>
+                <div class="row mt-5 pt-5">
+                    <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                        <div class="booking">
+                            <div class="text-right">
+                                <div class="title-number mr-4"><img src="assets/img/booking/number-1.png" alt="01"></div>
+                                <div class="title-icon"><img src="assets/img/booking/logo-1.png" alt="Icon"></div>
+                            </div>
+                            <div class="text-title mt-3 mb-1 text-right">Explore Activity Options</div>
+                            <div class="text-body text-right">Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words
+                            </div>
+                        </div>
+                        <div class="booking mt-5">
+                            <div class="text-right">
+                                <div class="title-number mr-4"><img src="assets/img/booking/number-2.png" alt="02"></div>
+                                <div class="title-icon"><img src="assets/img/booking/logo-2.png" alt="Icon"></div>
+                            </div>
+                            <div class="text-title text-right mt-3 mb-1">Add to Activity Planner</div>
+                            <div class="text-body text-right">Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                        <div class="iphone"><img src="assets/img/booking/iphone.png" alt="Iphone"></div>
+                    </div>
+                    <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                        <div class="booking">
+                            <div>
+                                <div class="title-icon mr-4"><img src="assets/img/booking/logo-3.png" alt="Icon"></div>
+                                <div class="title-number"><img src="assets/img/booking/number-3.png" alt="01"></div>
+                            </div>
+                            <div class="text-title mt-3 mb-1">Activity Booking</div>
+                            <div class="text-body">Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words
+                            </div>
+                        </div>
+                        <div class="booking mt-5">
+                            <div
+                            ">
+                            <div class="title-icon mr-4"><img src="assets/img/booking/logo-4.png" alt="Icon"></div>
+                            <div class="title-number"><img src="assets/img/booking/number-4.png" alt="02"></div>
+                        </div>
+                        <div class="text-title mt-3 mb-1">Confirm Activity Plan</div>
+                        <div class="text-body">Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-4 mt-5">
+        <div class="promise-list">
+            <div class="text-title mt-5">User Promise</div>
+            <div class="dash"><img src="assets/img/promise/dash.png" alt="Dash"></div>
+            <div class="list mt-5">
+                <div class="logo"><img src="assets/img/promise/logo-1.png" alt="Logo"> </div>
+                <div class="text">Exciting Booking Experience</div>
+            </div>
+            <div class="list mt-3">
+                <div class="logo"><img src="assets/img/promise/logo-2.png" alt="Logo"> </div>
+                <div class="text">Variety of Options</div>
+            </div>
+            <div class="list mt-3">
+                <div class="logo"><img src="assets/img/promise/logo-3.png" alt="Logo"> </div>
+                <div class="text">Easy of Use</div>
+            </div>
+            <div class="list mt-3">
+                <div class="logo"><img src="assets/img/promise/logo-4.png" alt="Logo"> </div>
+                <div class="text">Great Side Deals</div>
+            </div>
+        </div>
+
+    </section>
+
+
 <?php include_once "footer.php"; ?>
